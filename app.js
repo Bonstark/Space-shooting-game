30d0e0a21a79e65f0458d87f3f02fafb3259fc6a
Bonstark <31876796+Bonstark@users.noreply.github.com>
2017-11-22 18:31:27 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
-    // $('#container').append('<div id="timeChoices"><h3>How long would you like to play for?</h3></div>');
-    $('#container').append('<input type="button" class="display start" id="start" value="start" onClick="startGame()" />');
+    gameTime = 10000; // default time set to ten seconds
+    $('#container').append('<div id="timeChoices"><h3>How long would you like to play for?</h3></div>');
+    $('#container').append('<input type="button" class="display button selected" id="10s" value="10 seconds" onClick="chooseTime10()">');
+    $('#container').append('<input type="button" class="display button " id="30s" value="30 seconds" onClick="chooseTime30()">');
+    $('#container').append('<input type="button" class="display button " id="1m" value="1 minute" onClick="chooseTime1m()">');
+    $('#container').append('<input type="button" class="display  start" id="start" value="start" onClick="startGame()" >');
+    $(".button").on("click", function() {
+      $(".button").removeClass("selected");
+      $(this).addClass("selected"); //applies the selected class, used by the following global.
+      });
 })
-
 function startGame(){
   score = 0;
-  gameTime =  10000; //this is how many miliseconds the game will take. Change is passed to the gameTimer div.
-  counter = gameTime*0.03; //
+  //this is how many miliseconds the game will take. Change is passed to the gameTimer div.
+  counter = gameTime*0.03333; //
   mouseMovements();
 
-  $('#start, #timeChoices, #title').css("display", "none");
+  $('#start, #timeChoices, #title, #10s, #30s, #1m').css("display", "none");
   $('.hidden').show();
   setTimeout(endGame, gameTime);
   timer = setInterval(function() {
@@ -24,6 +31,26 @@ function startGame(){
         };
     }, 33);
 }
+function countdown(){
+  counter--;
+  var secondsRemaining = Math.round(counter/33.33);
+  $('#gameTimer').empty().append("<h3>" + secondsRemaining + " seconds left</h3>");
+  }
+function spawnAlien(){
+      $('#container').append($('#alienDiv'));
+      var docHeight = $('#container').height(),
+          docWidth = $('#container').width(),
+          $div = $('#alienDiv'),
+          divWidth = $div.width(),
+          divHeight = $div.height(),
+          heightMax = docHeight - divHeight,
+          widthMax = docWidth - divWidth;
+
+      $div.css({
+          left: Math.floor( Math.random() * widthMax ),
+          top: Math.floor( Math.random() * heightMax )
+        });
+  }
 function endGame(){
   clearInterval(timer);
   $('#spaceship1, #rocket, #alienDiv, #score, #scoreboard').hide();
@@ -31,28 +58,6 @@ function endGame(){
   $('#container').append('<div class="display" id="final"><h3>You killed ' + score + ' aliens.</h3></div>');
   $('#container').append('<input type="button" class="display start" id="restart" value="Menu" onClick="reloadGame()" />');
 }
-function countdown(){
-  counter--;
-  var secondsRemaining = Math.round(counter/33.33);
-  $('#gameTimer').empty().append("<h3>" + secondsRemaining + " seconds left</h3>");
-  }
-function collision(cOne, cTwo) {
-      var x1 = cOne.offset().left;
-      var y1 = cOne.offset().top;
-      var h1 = cOne.outerHeight(false);
-      var w1 = cOne.outerWidth(false);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = cTwo.offset().left;
-      var y2 = cTwo.offset().top;
-      var h2 = cTwo.outerHeight(true);
-      var w2 = cTwo.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
-
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return true;
-    }
 function mouseMovements(){
   $(document).mousemove(function(e){
      var box=$(".spaceship1");
@@ -78,21 +83,32 @@ function fireRocket(){
     $('#rocket').empty();
   });
 }
-function spawnAlien(){
-    $('#container').append($('#alienDiv'));
-    var docHeight = $('#container').height(),
-        docWidth = $('#container').width(),
-        $div = $('#alienDiv'),
-        divWidth = $div.width(),
-        divHeight = $div.height(),
-        heightMax = docHeight - divHeight,
-        widthMax = docWidth - divWidth;
+function collision(cOne, cTwo) {
+      var x1 = cOne.offset().left;
+      var y1 = cOne.offset().top;
+      var h1 = cOne.outerHeight(false);
+      var w1 = cOne.outerWidth(false);
+      var b1 = y1 + h1;
+      var r1 = x1 + w1;
+      var x2 = cTwo.offset().left;
+      var y2 = cTwo.offset().top;
+      var h2 = cTwo.outerHeight(true);
+      var w2 = cTwo.outerWidth(true);
+      var b2 = y2 + h2;
+      var r2 = x2 + w2;
 
-    $div.css({
-        left: Math.floor( Math.random() * widthMax ),
-        top: Math.floor( Math.random() * heightMax )
-      });
-}
-function reloadGame(){
-    location.reload();
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+      return true;
+    }
+function chooseTime10(){
+    gameTime = 10000
   }
+function chooseTime30(){
+    gameTime = 30000;
+  }
+function chooseTime1m(){
+    gameTime = 60000;
+  }
+function reloadGame(){
+      location.reload();
+    }
