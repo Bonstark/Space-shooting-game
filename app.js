d9ca647fbbed0176cfe038fef987326335f5e53a
Bonstark <31876796+Bonstark@users.noreply.github.com>
2017-11-22 13:43:21 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function startGame(){
-
-    var score = 0;
+  score = 0;
+  mouseMovements();
+  countdown(); //You can change how many seconds you have left in the timeout below, remember to change the time in function countdown too.
   $('#start').css("display", "none");
   $('.hidden').show();
-  countdown(); //You can change how many seconds you have left in the function line. You must also change the timeout below.
-    timer = setInterval(function() {
-        window.setTimeout(endGame, 10000);
+  setTimeout(endGame, 5000);
+  timer = setInterval(function() {
         $("#score").text("You score is " + score + " so far.");
         var alienCollision =  collision($("#rocket"), $("#alienDiv"));
-        console.log(alienCollision);
+        console.log("There is a collision. " + alienCollision);
         if (alienCollision === true) {
         $("#rocket").empty();
         spawnAlien();
@@ -16,43 +16,15 @@ function startGame(){
         };
     }, 33);
 }
-
-$(document).ready(function(){
-  $(document).mousemove(function(e){
-     var box=$(".spaceship1");
-     var boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
-     var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);
-     var angle = angle-180
-     box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});
-     box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
-     box.css({ 'transform': 'rotate(' + angle + 'deg)'});
-   });
-  $(document).mousedown(function(){
-    $("#spaceship1").removeClass('spaceship').addClass('spaceshipFire');
-    $("#rocket").append($('<img src="' + "https://i.imgur.com/AjHoktj.png" + '" />'));
-    $('#rocket').animate({top: '580px'}, "slow", function () {
-      $(this).removeAttr('style');
-      $('#rocket').empty();
-    });
-    });
-  $(document).mouseup(function(){
-      $("#spaceship1").removeClass('spaceshipFire').addClass('spaceship');
-    });
-});
-
 function endGame(){
-  $('#container').empty();
-  $('#score').hide();
+  clearInterval(timer);
+  $('#spaceship1, #rocket, #alienDiv, #score').hide();
   $('#title').empty().append("<h3>SPACE BLASTINGS</h3>");
-  $('#final').append("<h3>Your score was " + score + "</h3>");
-  // $('#score').removeClass('score').addClass('start');
-  // clearInterval(timer);
-  // alert('FINISH');
-  // $('.spaceship', '.rocket', '.alien').css("display", "none");
+  $('#container').append('<div class="display" id="final"><h3>Your score was ' + score + '</h3></div>');
 }
-
 function countdown(){
-  var counter = 10;
+  var counter = 5;
+  $('#title').empty().append("<h3>You have " + counter + " seconds left.</h3>");
   id = setInterval(function() {
     counter--;
     if(counter < 0) {
@@ -79,7 +51,30 @@ function collision(cOne, cTwo) {
       if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
       return true;
     }
-function firerocket(){
+function mouseMovements(){
+  $(document).mousemove(function(e){
+     var box=$(".spaceship1");
+     var boxCenter=[box.offset().left+box.width()/2, box.offset().top+box.height()/2];
+     var angle = Math.atan2(e.pageX- boxCenter[0],- (e.pageY- boxCenter[1]) )*(180/Math.PI);
+     var angle = angle-180
+     box.css({ "-webkit-transform": 'rotate(' + angle + 'deg)'});
+     box.css({ '-moz-transform': 'rotate(' + angle + 'deg)'});
+     box.css({ 'transform': 'rotate(' + angle + 'deg)'});
+   });
+  $(document).mousedown(function(){
+    $("#spaceship1").removeClass('spaceship').addClass('spaceshipFire');
+    fireRocket();
+    });
+  $(document).mouseup(function(){
+      $("#spaceship1").removeClass('spaceshipFire').addClass('spaceship');
+    });
+}
+function fireRocket(){
+  $("#rocket").append($('<img src="' + "https://i.imgur.com/AjHoktj.png" + '" />'));
+  $('#rocket').animate({top: '580px'}, "slow", function () {
+    $(this).removeAttr('style');
+    $('#rocket').empty();
+  });
 }
 function spawnAlien(){
     $('#container').append($('#alienDiv'));
